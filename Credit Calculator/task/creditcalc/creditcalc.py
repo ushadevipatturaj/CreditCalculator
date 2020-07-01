bbfd81613bc69e20092b1cfd1f3cada6f0668abb
ushapatturaj <ushadevipatturaj@gmail.com>
2020-07-01 22:14:04 +0200

    Credit Calculator

---
+++ Credit Calculator/task/creditcalc/creditcalc.py
@@ -0,0 +1,8 @@
+credit_principal = 'Credit principal: 1000'
+final_output = 'The credit has been repaid!'
+first_month = 'Month 1: paid out 250'
+second_month = 'Month 2: paid out 250'
+third_month = 'Month 3: paid out 500'
+
+# write your code here
+print("{}\n{}\n{}\n{}\n{}".format(credit_principal, first_month, second_month, third_month, final_output))
